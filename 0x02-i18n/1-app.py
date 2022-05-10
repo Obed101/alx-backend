ba661f087c647b45869a48c25096bae261f1fcfe
Obed Amoako <obedamoako92@gmail.com>
2022-05-10 14:44:23 +0000

    app running with Babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,7 +6,8 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 LANGUAGES = ["en", "fr"]
-
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 
 
 @app.route('/')
@@ -15,10 +16,10 @@ def index():
     return render_template("0-index.html")
 
 @babel.localeselector
+@app.route('/locale')
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
-    app.run(index())
-# add to you main app code
+    app.run()
