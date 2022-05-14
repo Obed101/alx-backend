ff69ac7dc06f16612efb0ea4091c4cf9b615edcd
Obed Amoako <obedamoako92@gmail.com>
2022-05-14 10:49:28 +0000

    fixed non int error

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -34,7 +34,7 @@ def get_locale() -> str:
 def get_user():
     """Returns a user dictionary or none"""
     user = request.args.get('login_as')
-    return users.get(user)
+    return users.get(int(user)) if user else None
 
 @app.before_request
 def before_request():
