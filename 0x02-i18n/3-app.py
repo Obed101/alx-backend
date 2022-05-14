e0a99f418a1830d33e1dd6a53d7d45087686766f
Obed Amoako <obedamoako92@gmail.com>
2022-05-14 08:48:18 +0000

    created locale file

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This module implements python flask_babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config['LANGUAGES'] = ["en", "fr"]
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+
+
+@app.route('/')
+def index() -> str:
+    """Returns the home page"""
+    return render_template("2-index.html")
+
+
+@babel.localeselector
+@app.route('/locale')
+def get_locale() -> str:
+    """Gets the locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
