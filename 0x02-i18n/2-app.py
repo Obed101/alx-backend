cf283bb4a1017569fbd7080864386759739459ec
Obed Amoako <obedamoako92@gmail.com>
2022-06-01 09:47:36 +0000

    fixed decorator problem

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,8 +16,8 @@ def index() -> str:
     return render_template("2-index.html")
 
 
-@babel.localeselector
 @app.route('/locale')
+@babel.localeselector
 def get_locale() -> str:
     """Gets the locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
