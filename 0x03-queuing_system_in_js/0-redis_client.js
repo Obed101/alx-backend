e3a4b04b67ac29a651bce5c4f40a93d65c7baf20
Obed101 <obedamoako92@gmail.com>
2022-06-07 16:40:20 +0000

    modified gitignore

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,15 @@
 // import { createClient } from 'redis';
-async function reddis() {
-    createClient = require('redis')
+async function _reddis () {
+  try {
+    let createClient = require('redis');
     createClient = createClient.createClient;
     const client = createClient();
     client.on('error', () => console.log('An error occured'));
     await client.connect();
     await client.set('name', 'Obed Amoako');
     console.log(await client.get('name'));
+  } catch (err) {
+    console.log(err);
+  }
 }
-reddis()
+_reddis();
